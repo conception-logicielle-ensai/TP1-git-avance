9878bc62f2540830bb2cbbd6f386d68843875894
Antoine Brunetti <antoine.brunetti@insee.fr>
2018-01-05 17:45:31 +0100

    ajout zonage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+import pytz # $ pip install pytz
 
-current_time = datetime.now()
+timezone = pytz.timezone('America/New_York')
+current_time = datetime.now(timezone)
 current_time_formatted = current_time.strftime("%H:%M:%S")
 print(current_time_formatted)
